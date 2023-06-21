408ea419208bce5b9ebe6738011832fafd5fe476
MUHAMAD AKMAL ADHA BIN RADZUAN <134881143+NotAjumma@users.noreply.github.com>
2023-06-21 12:36:28 +0800

    update baseURL

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public $baseURL = 'https://notajumma.github.io/melody/public/';
+    public $baseURL = 'https://notajumma.github.io/melody/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
